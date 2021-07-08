f775579de7d13c4f03e6d54400f8e7c2a18b9e01
finabluma <oscar.francino@finabluma.com>
2021-07-08 22:30:43 +0200

    Query Graphql Data

---
+++ src/views/Rockets.vue
@@ -0,0 +1,15 @@
+<script>
+import RocketsList from "../components/RocketsList.vue";
+export default {
+  components: { RocketsList },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+<template>
+  <div class="rockets">
+    <rockets-list />
+  </div>
+</template>
