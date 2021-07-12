dbe4b85602eafc607ced7be77998c57919e790fc
finabluma <oscar.francino@finabluma.com>
2021-07-12 19:15:04 +0200

    dynamic arguments

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/rockets">Rockets</router-link>
+      <router-link to="/rockets">Rockets</router-link> 
     </div>
     <router-view/>
   </div>
---
+++ src/views/Rocket.vue
@@ -0,0 +1,15 @@
+<script>
+import RocketItem from "../components/RocketItem.vue";
+export default {
+  components: { RocketItem },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+<template>
+  <div class="rockets">
+    <rocket-item />
+  </div>
+</template>
